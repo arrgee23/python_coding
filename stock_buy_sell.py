d706d16f5a7b71b73d8497f2b3d9690b4edd4b8e
Rahul Gautam <arr.gee23@gmail.com>
2019-10-12 08:13:12 +0530

    stock buy sell

--- stock_buy_sell.py
+++ stock_buy_sell.py
@@ -36,3 +36,43 @@ Testcase 1: We can buy stock on day 0, and sell it on 3rd day,
 Note: Output format is as follows - (buy_day sell_day) (buy_day sell_day)
 For each input, output should be in a single line.
 """
+
+
+def calculate(arr,l):
+    i=0
+    outcount = 0
+    while(i<=l-1):
+        #decide the buy day
+        #price falling
+        while(i<l-1 and arr[i]>=arr[i+1]):
+            i+=1
+        if(i>=l-1):
+            break
+
+        
+        mini=i
+
+        #decide the sell day
+        i+=1
+        while(i<l and arr[i]>=arr[i-1]):
+            i+=1
+        if(i-1>l-1):
+            break
+        maxi = i-1
+        print('(',mini,' ',maxi,')',end=' ',sep='')
+        outcount += 1
+    
+    if(outcount==0):
+        print("No Profit",end='\n')
+    else:
+        print("",end='\n')
+
+
+from sys import stdin,stdout
+
+t = int(stdin.readline())
+for i in range(t):
+    l = int(stdin.readline())
+    arr = [int(n) for n in stdin.readline().strip().split()]
+    calculate(arr,l)
+    
